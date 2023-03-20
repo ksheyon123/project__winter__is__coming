d2ed31d917524be1ad37ae59721f23ae15dc2cc4
Kangseohyeon <shkang@iconloop.com>
2023-03-20 20:36:46 +0900

    Need to change three js to @react-three library

--- src/hooks/usePlanet.tsx
+++ src/hooks/usePlanet.tsx
@@ -1,22 +1,25 @@
-import { useEffect, RefObject, useCallback } from "react";
+import { useState, useCallback } from "react";
 import * as THREE from "three";
 
 const usePlanet = () => {
+  const [renderer, setRenderer] = useState<any>();
 
   const initPlanet = useCallback((element: HTMLDivElement) => {
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    const renderer = new THREE.WebGLRenderer();
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+
     renderer.setSize(window.innerWidth, window.innerHeight);
     const renderJSX = renderer.domElement;
     if (element) {
       element.appendChild(renderJSX);
     }
-    scene.add(cube);
+    const geometry = new THREE.CircleGeometry(5, 32);
+    const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+    const circle = new THREE.Mesh(geometry, material);
+    scene.add(circle);
   }, []);
+
 
   return {
     initPlanet,
